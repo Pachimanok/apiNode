f5a3a8dad35d28c28835eed655039a6be1722df4
Pablo Rio <pablorio@MacBook-Air-de-Pablo.local>
2022-03-16 10:16:11 -0300

    Api para probar

--- api/product.js
+++ api/product.js
@@ -7,19 +7,40 @@ const mysqlConnection = require('../database');
  *
  * @return product list | empty.
  */
-router.get("/", async (req, res) => {
-  try {
-    mysqlConnection.query('SELECT * FROM posts', (err, rows, fields) => {
+router.get("/:u", async (req, res) => {
+
+  const { u } = req.params;
+
+  if( u != null ){
+
+    try {
+      mysqlConnection.query('SELECT posts.id, posts.name,posts.subtitulo,posts.body,images.url,categories.nombre FROM posts INNER JOIN categories ON categories.id = posts.category_id INNER JOIN images ON images.imageable_id = posts.id WHERE posts.user_id = ' + u , (err, rows, fields) => {
+        if(!err){
+            res.json(rows);
+        } else {
+              console.log(err);
+        }
+    });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).send("Server error");
+    } 
+  } 
+  
+});
+router.get('/:id/:u', (req, res) => {
+
+  const { id } = req.params;
+  const { u } = req.params;
+
+  mysqlConnection.query('SELECT * FROM posts WHERE id=? AND user_id = ?' ,[id, u],(err, rows,fields) =>{
       if(!err){
-          res.json(rows);
+          console.log(id);
+          res.json(rows[0]);
       } else {
             console.log(err);
       }
   });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send("Server error");
-  }
 });
 
 module.exports = router;
